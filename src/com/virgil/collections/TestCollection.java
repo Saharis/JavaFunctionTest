deea462821ece19bb6f9b90f016130fb8c94e3a3
Virgil <saharis1990@gmail.com>
2015-01-09 21:36:15 +0800

    sync

---
+++ src/com/virgil/collections/TestCollection.java
@@ -0,0 +1,15 @@
+package com.virgil.collections;
+
+import java.util.*;
+
+/**
+ * Created by virgil on 15-1-6.
+ */
+public class TestCollection {
+
+    public static void main(String[] args){
+        Collection<String> cls=new ArrayList<>();
+//        LinkedList
+                PriorityQueue
+    }
+}
